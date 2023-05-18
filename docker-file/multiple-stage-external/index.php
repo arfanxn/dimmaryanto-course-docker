defcd8324c3d7b406f70af44e31939de51ad997c
arfanxn <arfan2173@gmail.com>
2023-05-19 00:07:34 +0700

    learn dockerfile multi staging external from

---
+++ docker-file/multiple-stage-external/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<title>Belajar HTML</title>
+
+		<!--Import Google Icon Font-->
+		<link
+			href="https://fonts.googleapis.com/icon?family=Material+Icons"
+			rel="stylesheet"
+		/>
+		<!-- Compiled and minified CSS -->
+		<link
+			rel="stylesheet"
+			href="node_modules/materialize-css/dist/css/materialize.min.css"
+		/>
+
+		<!--Let browser know website is optimized for mobile-->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	</head>
+
+	<body>
+		<nav class="blue">
+			<div class="nav-wrapper container">
+				<a href="#" class="brand-logo">Webapp Cources</a>
+				<ul id="nav-mobile" class="right hide-on-med-and-down">
+					<li><a href="sass.html">HTML</a></li>
+					<li><a href="badges.html">CSS</a></li>
+					<li><a href="collapsible.html">JavaScript</a></li>
+				</ul>
+			</div>
+		</nav>
+
+		<div>
+			<div class="row">
+				<div class="col s12 m6">
+					<div class="card blue-grey darken-1">
+						<div class="card-content white-text">
+							<span class="card-title">HTML</span>
+							<p>
+								I am a very simple card. I am good at containing
+								small bits of information. I am convenient
+								because I require little markup to use
+								effectively.
+							</p>
+						</div>
+						<div class="card-action">
+							<a href="#">This is a link</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Compiled and minified JavaScript -->
+		<script
+			type="text/javascript"
+			src="node_modules/jquery/dist/jquery.min.js"
+		></script>
+		<script
+			type="text/javascript"
+			src="node_modules/materialize-css/dist/js/materialize.min.js"
+		></script>
+	</body>
+</html>
